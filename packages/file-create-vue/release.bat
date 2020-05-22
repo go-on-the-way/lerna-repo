18941b0bcabc7a53489050cfec86aa982a27b9ec
wanglei <wlei-wang@starlinke.com>
2020-05-22 19:49:39 +0800

    修改

--- packages/file-create-vue/release.bat
+++ packages/file-create-vue/release.bat
@@ -5,11 +5,11 @@ ECHO "============================= RELEASE START..."
 SET version=1.0.0
 
 :: commit
-git add -A
-git commit -m "release branch %version%"
-npm version %version% --message "release %version%"
+:: git add -A
+:: git commit -m "release branch %version%"
+:: npm version %version% --message "release %version%"
 
 :: publish
-git push
+:: git push
 npm login
 npm publish
